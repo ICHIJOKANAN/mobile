941756503510aa9e4f1129bf7b554aa13dfd2ae2
ICHIJOKANAN <zeal23310091@fsgcl.jp>
2026-06-10 14:46:53 +0900

    メイン画面を修正しました

--- lib/list/MainContent.dart
+++ lib/list/MainContent.dart
@@ -87,18 +87,18 @@ class _MainContentState extends State<MainContent> {
                       ),
                     );
                   },
-                  icon: const Icon(Icons.pie_chart),
+                  //icon: const Icon(Icons.pie_chart),
                   label: const Text('合計・グラフを見る'),
                   style: ButtonStyle(
                     backgroundColor: WidgetStateProperty.all<Color>(Colors.purple),
                     foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-                    minimumSize: WidgetStateProperty.all<Size>(const Size(200, 60)),
+                    fixedSize: WidgetStateProperty.all<Size>(const Size(200, 60),),
                     textStyle: WidgetStateProperty.all<TextStyle>(
                       const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
-                const SizedBox(height: 12),
+                const SizedBox(height: 16),
                 ElevatedButton.icon(
                   onPressed: () {
                     Navigator.push(
@@ -108,12 +108,12 @@ class _MainContentState extends State<MainContent> {
                       ),
                     );
                   },
-                  icon: const Icon(Icons.list),
+                  //icon: const Icon(Icons.list),
                   label: const Text('詳細な記録を見る'),
                   style: ButtonStyle(
                     backgroundColor: WidgetStateProperty.all<Color>(Colors.teal),
                     foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-                    minimumSize: WidgetStateProperty.all<Size>(const Size(200, 60)),
+                    fixedSize: WidgetStateProperty.all<Size>(const Size(200, 60),),
                     textStyle: WidgetStateProperty.all<TextStyle>(
                       const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
